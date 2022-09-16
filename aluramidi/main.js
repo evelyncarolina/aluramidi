f2025ffbd95f16dd46782501b21eeb0f4f450150
evelyncarolina <evelyncarolina.contato@gmail.com>
2022-09-16 15:24:53 -0300

    [Feature - Task: Melhorando o código usando for]

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -19,7 +19,7 @@ function tocaSom (seletorAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
+/**
 let contador = 0;
 
 while (contador < listaDeTeclas.length){
@@ -36,5 +36,29 @@ while (contador < listaDeTeclas.length){
     contador = contador + 1;
     //console.log(contador);
 }
+ */
 
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+       
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
+
+}
 
