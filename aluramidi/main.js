7aaa86a790cff66259fed8ec696d62a7acf740d1
evelyncarolina <evelyncarolina.contato@gmail.com>
2022-09-16 15:21:05 -0300

    [Feature - TasK: Aprendendo novos comandos de códigos]

---
+++ aluramidi/main.js
@@ -0,0 +1,6 @@
+// document = acessa o documento html 
+// querySelector = busca o seletor do elemento informado
+// retorna o que foi informado exemplo id, classe, elemento
+// function sempre tem que ter um NOME
+
+
