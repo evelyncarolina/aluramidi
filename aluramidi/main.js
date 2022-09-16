17458c4d0f7ce690625c4be1468bb78034180f9b
evelyncarolina <evelyncarolina.contato@gmail.com>
2022-09-16 15:23:24 -0300

    [Feature - Task: Aprendedo a usar variavel let]

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -3,4 +3,38 @@
 // retorna o que foi informado exemplo id, classe, elemento
 // function sempre tem que ter um NOME
 
+function tocaSom (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado!');
+    }
+}
+
+// //#onclick é um atributo então precisa passar um valor
+// se que queremos guardar uma função dentro de um atributo "ONCLICK" precisa retirar os parenteses "()"
+// #const é uma variavel que o valor é constante, não pode ser alterado
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    
+    contador = contador + 1;
+    //console.log(contador);
+}
+
 
